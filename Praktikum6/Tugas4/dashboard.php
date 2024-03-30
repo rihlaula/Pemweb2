587d1caf1b00d4046eeb62553d1d504706ab9569
rihlaula <rihlarahmat20@gmail.com>
2024-03-30 14:42:35 +0700

    Praktikum6

---
+++ Praktikum6/Tugas4/dashboard.php
@@ -0,0 +1,27 @@
+<?php
+include_once('top2.php');
+include_once('menu2.php');
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="">HALLO!</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Dashboard</li>
+          </ol>
+        </div>
+      </div>
+      <h3>Selamat Datang di Halaman Admin</h3>
+      </section>
+    </div>
+
+    <?php
+    include_once('bottom2.php');
+    ?>
---
+++ Praktikum6/Tugas4/proses_registrasi.php
@@ -0,0 +1,62 @@
+<?php
+//Tangkap imput form
+$nim = $_POST['nim'];
+$nama = $_POST['nama'];
+$jk = $_POST['jk'];
+$prodi = $_POST['prodi'];
+$skills = $_POST['skills'];
+$domisili = $_POST['domisili'];
+$email = $_POST['email'];
+
+$nilai_skills = [
+    'HTML' => 10,
+    'CSS' => 10,
+    'JavaScript' => 20,
+    'RWD Bootstrap' => 20,
+    'PHP' => 30,
+    'Python' => 30,
+    'Java' => 50,
+];
+
+$skor_skill = 0;
+foreach ($skills as $value) {
+    $skor_skill += $nilai_skills[$value];
+};
+
+//hitung kategori 
+function skor_skill($skor_skill)
+{
+    if ($skor_skill >= 0 && $skor_skill <= 40) {
+        return "Kurang";
+    } elseif ($skor_skill >= 41 && $skor_skill <= 60) {
+        return "Cukup";
+    } elseif ($skor_skill >= 61 && $skor_skill <= 100) {
+        return "Baik";
+    } elseif ($skor_skill >= 101 && $skor_skill <= 150) {
+        return "Sangat Baik";
+    } else {
+        return "Tidak memadai";
+    }
+};
+
+echo "NIM: $nim";
+echo "<br>Nama Lengkap: $nama";
+echo "<br>Jenis Kelamin: $jk";
+echo "<br>Program Study: $prodi";
+echo "<br>Skills: " . join(', ', $skills);
+echo "<br>Tempat Domisili: $domisili";
+echo "<br>Email: $email";
+echo "<br>Skor: $skor_skill";
+echo "<br>Kategori Skill: " . skor_skill($skor_skill);
+?>
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+        </div>
+
+    
